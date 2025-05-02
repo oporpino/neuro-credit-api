e48e43b867b01d6e77ae849a2aafb61b227808d0
o_porpino <dev@oporpino.com>
2025-05-02 19:20:18 -0300

    chore: config Swagger for documentation

---
+++ src/main/java/br/com/neurotech/challenge/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package br.com.neurotech.challenge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.neurotech.challenge.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Neurotech Credit API")
+                .description("API for credit evaluation and management")
+                .version("1.0.0")
+                .build();
+    }
+}
