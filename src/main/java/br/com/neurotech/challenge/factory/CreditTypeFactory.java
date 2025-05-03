01af7185f99848ce02535bb3c7d6ffd3c54e697e
o_porpino <dev@oporpino.com>
2025-05-02 21:43:27 -0300

    refactor: move CreditTypeFactory to factory package

--- src/main/java/br/com/neurotech/challenge/factory/CreditTypeFactory.java
+++ src/main/java/br/com/neurotech/challenge/factory/CreditTypeFactory.java
@@ -1,5 +1,8 @@
-package br.com.neurotech.challenge.entity;
+package br.com.neurotech.challenge.factory;
 
+import br.com.neurotech.challenge.entity.CreditType;
+import br.com.neurotech.challenge.entity.NeurotechClient;
+import br.com.neurotech.challenge.entity.VehicleModel;
 import br.com.neurotech.challenge.entity.credit.Credit;
 
 public class CreditTypeFactory {
